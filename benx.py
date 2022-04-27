2476475e8fd52605675848e3b25e7177c2238938
soilmo <60858896+soilmo@users.noreply.github.com>
2022-04-27 01:53:06 -0300

    Add files via upload

--- benx.py
+++ benx.py
@@ -23,7 +23,39 @@ st.title("Indica Leopoldina")
 
 st.markdown("Encontre as melhores indicações da região :smile:")
 
+import email, smtplib, ssl
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+@st.cache(persist=True, max_entries = 20, ttl = 1800, show_spinner=False)
+def enviar_email(tipo, termo, pessoa):
+    
+    subject = "IL|"+str(tipo)+"|"+str(termo)+"|"+str(pessoa)
+    body = "IL|"+str(tipo)+"|"+str(termo)+"|"+str(pessoa)
+    sender_email = "pedrohs.t19@gmail.com"
+    receiver_email = "pedrohs.t19@gmail.com"
+    password = "291096santiago"
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+
+@st.cache(persist=True, max_entries = 20, ttl = 1800, show_spinner=False)
 def opcoes_resultado(aux, i):
     nome = aux['nome'].iloc[i]
     telefone = aux['telefone'].iloc[i]
@@ -37,90 +69,96 @@ def opcoes_resultado(aux, i):
 categorias =list(df['categoria'].unique())
 categorias.sort()
 
-    
 categoria = st.selectbox("Qual categoria?", options=categorias, index = categorias.index("Lanches e doces"))
+filtro = df['categoria']==categoria
 
-with st.spinner("Carregando resultados..."):
-    filtro = df['categoria']==categoria
+if df[filtro].shape[0] == 1:
+    st.markdown("Temos " + str(df[filtro].shape[0]) + " indicação")
+else:
+    st.markdown("Temos " + str(df[filtro].shape[0]) + " indicações")
 
-    if df[filtro].shape[0] == 1:
-        st.markdown("Temos " + str(df[filtro].shape[0]) + " indicação para você")
-    else:
-        st.markdown("Temos " + str(df[filtro].shape[0]) + " indicações para você")
+aux = df[filtro]
+    
+for i in range(aux.shape[0]):
 
-    aux = df[filtro]
+    nome, telefone, zap, descricao, insta = opcoes_resultado(aux, i)
+    if zap != "nan" and zap == zap:
+        link_zap = 'https://api.whatsapp.com/send?phone=55'+str(zap)+'&text=Oi%20'+str(nome).replace(" ","%20")+'%2C%20te%20achei%20pelo%20Indica%20Leopoldina.%20Gostaria%20de%20saber%20mais%20sobre%20seu%20serviço%20de%20'+categoria.replace(" ","%20")
+        t = '*Mensagem no Zap*'
+        link_zap = f'[{t}]({link_zap})'
+    if insta != "nan" and insta == insta:
+        link_insta = 'https://www.instagram.com/'+str(insta)+'/'
+        t = '*Instagram*'
+        link_insta = f'[{t}]({link_insta})'
+    
+    
+    #with st.expander(label=nome, expanded=False):
+    if st.button(nome):
         
+        st.markdown("__Descrição:__ " + str(descricao))
+        
+        if telefone == telefone:
+                st.markdown("__Telefone:__ " + str(telefone))
+            
+        if zap != "nan" and zap == zap and insta != "nan" and insta == insta:
+            st.markdown(link_zap + " | " +link_insta, unsafe_allow_html=True)
+        elif (zap == "nan" or zap!=zap) and (insta != "nan" and insta == insta):
+            st.markdown(link_insta, unsafe_allow_html=True)
+        elif (zap != "nan" and zap == zap) and (insta == "nan" or insta != insta):
+            st.markdown(link_zap, unsafe_allow_html=True)
+        
+        enviar_email("categoria", categoria, nome)
+        
+            
+# Busca específica
+st.session_state.busca = st.text_input("Não encontrou o que queria? Digite a palavra chave de sua busca", "").lower()
+
+if st.session_state.busca != "":
+
+    resultados = []
+    for i in range(df.shape[0]):
+        desc = df['descricao'].iloc[i]
+        try:
+            if st.session_state.busca in desc.lower():
+                resultados.append(i)
+        except:
+            pass
+
+    if len(resultados) == 1:
+        st.markdown("Temos " + str(len(resultados)) + " resultado para você")
+    else:
+        st.markdown("Temos " + str(len(resultados)) + " resultados para você")
+
+    filtro = df.index.isin(resultados)
+    aux = df[filtro]
+
     for i in range(aux.shape[0]):
 
         nome, telefone, zap, descricao, insta = opcoes_resultado(aux, i)
         
-        if zap == zap:
-            link_zap = 'https://api.whatsapp.com/send?phone=55'+str(zap)+'&text=Oi%20'+str(nome).replace(" ","%20")+'%2C%20te%20achei%20pelo%20Indica%20Leopoldina.%20Gostaria%20de%20saber%20mais%20sobre%20seu%20serviço%20de%20'+categoria.replace(" ","%20")
+        if zap != "nan" and zap == zap:
+            link_zap = 'https://api.whatsapp.com/send?phone=55'+str(zap)+'&text=Oi%20'+str(nome).replace(" ","%20")+'%2C%20te%20achei%20pela%20Busca!%20Gostaria%20de%20saber%20mais%20sobre%20seu%20serviço%20de%20'+categoria.replace(" ","%20")
             t = '*Mensagem no Zap*'
             link_zap = f'[{t}]({link_zap})'
-        if insta == insta:
+        if insta != "nan" and insta == insta:
             link_insta = 'https://www.instagram.com/'+str(insta)+'/'
             t = '*Instagram*'
             link_insta = f'[{t}]({link_insta})'
 
-        with st.expander(label=nome, expanded=False):
-        
-            st.markdown("__Telefone:__ " + str(telefone))
+        if st.button(nome + " "):
             st.markdown("__Descrição:__ " + str(descricao))
-            if zap == zap and insta == insta:
-                st.markdown(link_zap + " | " +link_insta, unsafe_allow_html=True)
-            elif zap != zap and insta == insta:
-                st.markdown(link_insta, unsafe_allow_html=True)
-            elif zap == zap and insta != insta:
-                st.markdown(link_zap, unsafe_allow_html=True)
-                
-# Busca específica
-st.session_state.busca = st.text_input("Não encontrou o que queria? Digite a palavra chave de sua busca", "").lower()
-
-with st.spinner("Carregando resultados..."):
-
-    if st.session_state.busca != "":
-
-        resultados = []
-        for i in range(df.shape[0]):
-            desc = df['descricao'].iloc[i]
-            try:
-                if st.session_state.busca in desc.lower():
-                    resultados.append(i)
-            except:
-                pass
-
-        if len(resultados) == 1:
-            st.markdown("Temos " + str(len(resultados)) + " resultado para você")
-        else:
-            st.markdown("Temos " + str(len(resultados)) + " resultados para você")
-
-        filtro = df.index.isin(resultados)
-        aux = df[filtro]
-
-        for i in range(aux.shape[0]):
-
-            nome, telefone, zap, descricao, insta = opcoes_resultado(aux, i)
             
-            if zap == zap:
-                link_zap = 'https://api.whatsapp.com/send?phone=55'+str(zap)+'&text=Oi%20'+str(nome).replace(" ","%20")+'%2C%20te%20achei%20pela%20Busca!%20Gostaria%20de%20saber%20mais%20sobre%20seu%20serviço%20de%20'+categoria.replace(" ","%20")
-                t = '*Mensagem no Zap*'
-                link_zap = f'[{t}]({link_zap})'
-            if insta == insta:
-                link_insta = 'https://www.instagram.com/'+str(insta)+'/'
-                t = '*Instagram*'
-                link_insta = f'[{t}]({link_insta})'
-
-            with st.expander(label=nome, expanded=False):
-            
+            if telefone == telefone:
                 st.markdown("__Telefone:__ " + str(telefone))
-                st.markdown("__Descrição:__ " + str(descricao))
-                if zap == zap and insta == insta:
-                    st.markdown(link_zap + " | " +link_insta, unsafe_allow_html=True)
-                elif zap != zap and insta == insta:
-                    st.markdown(link_insta, unsafe_allow_html=True)
-                elif zap == zap and insta != insta:
-                    st.markdown(link_zap, unsafe_allow_html=True)
+
+            if zap != "nan" and zap == zap and insta != "nan" and insta == insta:
+                st.markdown(link_zap + " | " +link_insta, unsafe_allow_html=True)
+            elif (zap == "nan" or zap!=zap) and (insta != "nan" and insta == insta):
+                st.markdown(link_insta, unsafe_allow_html=True)
+            elif (zap != "nan" and zap == zap) and (insta == "nan" or insta != insta):
+                st.markdown(link_zap, unsafe_allow_html=True)
+            enviar_email("busca", categoria, nome)
+        
 
 
 t = "*Indicações, dúvidas ou sugestões? Fale com a gente :)*"
