215d08fa7ce251e450b56a19581990366690be3a
soilmo <60858896+soilmo@users.noreply.github.com>
2022-05-22 14:20:53 -0300

    Add files via upload

--- benx.py
+++ benx.py
@@ -8,10 +8,6 @@ import telebot
 api = '2033329178:AAFAzrTtJE3uYqAoyEBdMlns_GcLjpk2big'
 bot = telebot.TeleBot(api)
 
-# Ler base
-url = 'https://github.com/soilmo/Indica/raw/main/servicos.csv?raw=true'
-df = pd.read_csv(url, encoding='utf-8', sep = ";")
-
 # Mudar título
 st.set_page_config(page_title = "Indica Leopoldina", page_icon=":nerd_face:")
 
@@ -33,13 +29,21 @@ t = "*Indicações ou dúvidas? Fale com a gente :)*"
 link_duvidas = 'https://instagram.com/indicaleopoldina?igshid=YmMyMTA2M2Y='
 st.markdown(f'[{t}]({link_duvidas})', unsafe_allow_html=True)
 
+# Ler base
+@st.cache(persist=True, max_entries = 20, ttl = 1800, show_spinner=False, suppress_st_warning=True)
+def ler_base():
+    url = 'https://github.com/soilmo/Indica/raw/main/servicos.csv?raw=true'
+    df = pd.read_csv(url, encoding='utf-8', sep = ";")
+    print("Leu")
+    return df
+
 def enviar_msg_telegram(bot, tipo, termo, pessoa):
     #texto = "IL|"+str(tipo)+"|"+str(termo)+"|"+str(pessoa)+"|"+datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     texto = "IL|"+str(tipo)+"|"+str(termo)+"|"+str(pessoa)
     bot.send_message("1333490728", texto)
 
 
-@st.cache(persist=True, max_entries = 20, ttl = 1800, show_spinner=False)
+@st.cache(persist=True, max_entries = 20, ttl = 1800, show_spinner=False, suppress_st_warning=True)
 def opcoes_resultado(aux, i):
     nome = aux['nome'].iloc[i]
     telefone = aux['telefone'].iloc[i]
@@ -50,6 +54,13 @@ def opcoes_resultado(aux, i):
     descricao = aux['descricao'].iloc[i]
     insta = aux['instagram'].iloc[i]
     return nome, categoria, endereco, telefone, zap, descricao, insta
+
+
+# Carregar base
+df = ler_base()
+
+# Nova visita
+# enviar_msg_telegram(bot, "VISITA", "", "")
 
 
 # Buscar categoria
@@ -100,6 +111,8 @@ for i in range(aux.shape[0]):
         
             
 # Busca específica
+
+
 st.session_state.busca = st.text_input("Não encontrou o que queria? Digite a palavra chave de sua busca", "").lower().replace(" ","")
 
 if st.session_state.busca != "":
@@ -152,5 +165,4 @@ if st.session_state.busca != "":
                 st.markdown(link_zap, unsafe_allow_html=True)
             
             enviar_msg_telegram(bot, "busca", categoria_res, nome)
-        
         
